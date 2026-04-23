bb73814172c6d7388571c019a1ff4561b94ca767
anudda <choiseonghee9307@gmail.com>
2026-04-23 21:27:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ window.onload = () => changeImg('{encoded_photos[0]}', 0);
 </script>
 """
 
-components.html(album_html, height=0)
+components.html(album_html, height=510)
 
 # 6. 정보 섹션
 st.markdown("""
